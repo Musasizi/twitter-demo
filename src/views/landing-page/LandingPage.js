b0f2b8da454834a0d3e16f5470ba3dd7a0f8471f
Musasizi Kenneth <kennymusasizi@hotmail.com>
2023-03-22 10:08:09 +0300

    💫Responsiveness

--- src/views/landing-page/LandingPage.js
+++ src/views/landing-page/LandingPage.js
@@ -1,3 +1,5 @@
+import { Button, Grid } from "@mui/material";
+import AltRouteIcon from '@mui/icons-material/AltRoute';
 import GroupName from "../../components/group-name/GroupName"
 export default function LandingPaging() {
 
@@ -9,12 +11,70 @@ export default function LandingPaging() {
     ];
 
     return (
-        <div>
-            {
+        <div style={{ width: "50%", backgroundColor: "GrayText" }}>
+
+            <Grid container spacing={4} justifyContent="center" direction="row" alignItems="center">
+                <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+                <Grid item sm={12} md={6} lg={4}>
+                    <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                        <em>Click me</em>
+                    </Button>
+                </Grid>
+            </Grid>
+
+
+            {/* <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                <em>Click me</em>
+            </Button>
+            <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                <em>Click me</em>
+            </Button>
+            <Button variant="contained" color="primary" fullWidth startIcon=<AltRouteIcon /> >
+                <em>Click me</em>
+            </Button> */}
+
+
+
+
+            {/* {
                 groupNames.map((item, key) => {
                     return <GroupName key={key + 1} name={item.name} color={item.color} />
                 })
-            }
+            } */}
 
 
         </div>
